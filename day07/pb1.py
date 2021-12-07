4620c37e7219421e092d00089eb7d9b6460716a9
cobacdavid <david.cobac@gmx.fr>
2021-12-07 19:51:12 +0100

    day 7

---
+++ day07/pb1.py
@@ -0,0 +1,19 @@
+def main(fichier):
+    with open(fichier) as fh:
+        ma_liste = list(map(int, fh.readline().strip().split(',')))
+        M, m = max(ma_liste), min(ma_liste)
+
+        # position = m
+        somme_min = float('inf')
+        for p in range(m, M + 1):
+            somme = 0
+            for crabe in ma_liste:
+                somme += abs(crabe - p)
+            if somme < somme_min:
+                somme_min = somme
+                # position = p
+
+    return somme_min
+
+
+print(main("jeu2.txt"))
